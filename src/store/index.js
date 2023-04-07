b3d71149defe4d36a8ec70dffb3b610697fb0788
LaChancee <miharisoababef@gmail.com>
2023-04-07 15:22:46 +0200

    Fetch des info de l'api

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,19 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    astres:[]
   },
   getters: {
+    astres: (state) => state.astres
   },
   mutations: {
+    getAstres:(state) => {
+      fetch('https://api.le-systeme-solaire.net/rest/bodies/')
+          .then( (data) => data.json())
+          .then( (user) => {
+            state.astres = user.bodies;
+          })
+    }
   },
   actions: {
   },
