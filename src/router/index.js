5a3dff1fe6bfc6be016646834f610e8f6e8d4386
LaChancee <miharisoababef@gmail.com>
2023-06-07 10:19:49 +0200

    route vers le system solaire retirer

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AstresView from "@/views/AstresView.vue";
+import DetailView from "@/views/DetailView.vue";
+// import SolarSystemView from "@/views/SolarSystemView.vue";
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  // {
+  //   path: '/solar-system',
+  //   name: 'SolarSystemView',
+  //   component: SolarSystemView
+  // },
+  {
+    path: '/astres/:id',
+    name: 'AstreDetail',
+    component: DetailView
   },
   {
     path: '/astres',
