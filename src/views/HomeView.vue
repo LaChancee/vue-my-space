346e05660c1710ab4dbef9fd7ce93fa97489562b
LaChancee <miharisoababef@gmail.com>
2023-06-07 10:21:28 +0200

    Ajout des router link

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,16 +5,16 @@
       <div class="] mx-auto px-4 sm:px-6 lg:px-8 py-24 space-y-8">
         <!-- Announcement Banner -->
         <div class="flex justify-center">
-          <a class="group inline-block bg-white/[.05] hover:bg-white/[.1] border border-white/[.05] p-1 pl-4 rounded-full shadow-md" >
+          <router-link to="/solar-system" class="group inline-block bg-white/[.05] hover:bg-white/[.1] border border-white/[.05] p-1 pl-4 rounded-full shadow-md" >
             <p class="mr-2 inline-block text-white text-sm">
-              Découvreze les planètes grâce à cette nouvelle API
+              Découvrez le système solaire grâce à cette nouvelle API
             </p>
             <span class="group-hover:bg-white/[.1] py-2 px-3 inline-flex justify-center items-center gap-x-2 rounded-full bg-white/[.075] font-semibold text-white text-sm">
             <svg class="w-2.5 h-2.5" width="16" height="16" viewBox="0 0 16 16" fill="none">
               <path d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
             </svg>
           </span>
-          </a>
+          </router-link>
         </div>
         <!-- End Announcement Banner -->
 
